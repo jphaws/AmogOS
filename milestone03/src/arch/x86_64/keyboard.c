9a069cb921a52136b84bd546245e9feaa3aaa55d
jphaws <jphaws@calpoly.edu>
2022-04-12 01:02:11 -0700

    Started working on VGA functions

---
+++ milestone03/src/arch/x86_64/keyboard.c
@@ -0,0 +1,28 @@
+#include "keyboard.h"
+
+#define VGA_BASE 0xb8000
+
+static unsigned short *vgaBuff = (unsigned short*)VGA_BASE;
+static int width = 80;
+static int height = 20;
+static int cursor = 0;
+static unsigned char color = FG(VGA_LIGHT_GREY) | BG(VGA_BLACK);
+
+void VGA_clear(void);
+
+void VGA_display_char(char c){
+   if (c == '\n') {
+      cursor = (LINE(cursor) + 1) * width;
+      if (cursor >= width*height)
+         scroll();
+   }
+   else if (c == '\r')
+      cursor = LINE(cursor);
+   else {
+      vgaBuff[cursor] = (color << 8) | c;
+      if ( (cursor % width) < (width - 1))
+         cursor++;
+   }
+}
+
+void VGA_display_str(const char *);
---
+++ milestone03/src/arch/x86_64/keyboard.h
@@ -0,0 +1,3 @@
+void VGA_clear(void);
+void VGA_display_char(char);
+void VGA_display_str(const char *);
