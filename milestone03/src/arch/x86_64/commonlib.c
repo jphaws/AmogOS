b6db7f40ba1a9c626b9f92f603f99f995c4dcbe1
jphaws <jphaws@calpoly.edu>
2022-04-16 17:10:54 -0700

    Finished milestone 3

--- milestone03/src/arch/x86_64/commonlib.c
+++ milestone03/src/arch/x86_64/commonlib.c
@@ -1,12 +1,10 @@
 #include <stddef.h>
-#include <stdint.h>
+#include <stdint-gcc.h>
 #include "commonlib.h"
+
 
 void *memset(void *dst, int c, size_t n){
    uint8_t *reset = (uint8_t *)dst;
-
-   if (dst == NULL)
-      return NULL;
 
    for (int i = 0; i < n; i++)
       reset[i] = c;
@@ -17,8 +15,6 @@ void *memset(void *dst, int c, size_t n){
 void *memcpy(void *dest, const void *src, size_t n){
    uint8_t *old = (uint8_t *)src;
    uint8_t *new = (uint8_t *)dest;
-   if (dest == NULL || src == NULL || n == 0)
-      return dest;
 
    for (size_t i = 0; i < n; i++)
       new[i] = old[i];
@@ -29,23 +25,17 @@ void *memcpy(void *dest, const void *src, size_t n){
 size_t strlen(const char *s){
    size_t len = 0;
 
-   if (s == NULL)
-      return 0;
-
    for (size_t i = 0; i < SIZE_MAX; i++){
-      if (s[i] == '\0')
+      if (s[i] == '\0'){
          len = i;
          break;
+      }
    }
 
    return len;
 }
 
 char *strcpy(char *dest, const char *src){
-   if (dest == NULL || src == NULL){
-      return (char *)src;
-   }
-
    for (size_t i = 0; i < SIZE_MAX; i++){
       dest[i] = src[i];
       if (src[i] == '\0')
@@ -61,4 +51,239 @@ const char *strchr(const char *s, int c);
 
 char *strdup(const char *s);
 
-int printk(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
+/*
+ * Converts an int to a string
+ */
+void itoa(int value, char *str){
+   int digits = 12;     // -2147483648 -> 2147483647
+   char rev[digits];
+   rev[0] = '\0';
+   int len = 0;
+
+   if (value == 0){
+      str[0] = '0';
+      str[1] = '\0';
+      return;
+   }
+
+   if (value < 0){
+      itoa(value * -1, str+1);
+      str[0] = '-';
+
+   }
+   else{
+      for (int i = 1; value > 0 && i < digits; i++){
+         rev[i] = value % 10;
+         value /= 10;
+         len++;
+      }
+
+      for (int i = 0; i < len; i++)
+         str[i] = '0' + rev[len - i];
+      str[len] = '\0';
+   }
+}
+
+/*
+ * Converts a long to a string
+ */
+void ltoa(long value, char *str){
+   int digits = 21;     // -9223372036854775808 -> 9223372036854775807 	
+   char rev[digits];
+   rev[0] = '\0';
+   int len = 0;
+
+   if (value == 0){
+      str[0] = '0';
+      str[1] = '\0';
+      return;
+   }
+
+   if (value < 0){
+      ltoa(value * -1, str+1);
+      str[0] = '-';
+
+   }
+   else{
+      for (int i = 1; value > 0 && i < digits; i++){
+         rev[i] = value % 10;
+         value /= 10;
+         len++;
+      }
+
+      for (int i = 0; i < len; i++)
+         str[i] = '0' + rev[len - i];
+      str[len] = '\0';
+   }
+}
+
+/*
+ * Converts an unsigned long to a string
+ */
+void lutoa(unsigned long value, char *str){
+   int digits = 21;     // 0 -> 18446744073709551615
+   char rev[digits];
+   rev[0] = '\0';
+   int len = 0;
+
+   if (value == 0){
+      str[0] = '0';
+      str[1] = '\0';
+      return;
+   }
+
+   for (int i = 1; value > 0 && i < digits; i++){
+      rev[i] = value % 10;
+      value /= 10;
+      len++;
+   }
+
+   for (int i = 0; i < len; i++)
+      str[i] = '0' + rev[len - i];
+   str[len] = '\0';
+}
+
+/* 
+ * Converts an unsigned int to a string
+ */
+void utoa(unsigned char value, char *str){
+   int digits = 11; // 0 -> 256
+   char rev[digits];
+   rev[0] = '\0';
+   int len = 0;
+
+   if (value == 0){
+      str[0] = '0';
+      str[1] = '\0';
+      return;
+   }
+
+   for (int i = 1; value > 0 && i < digits; i++){
+      rev[i] = value % 10;
+      value /= 10;
+      len++;
+   }
+
+   for (int i = 0; i < len; i++)
+      str[i] = '0' + rev[len - i];
+   str[len] = '\0';
+}
+
+/* 
+ * Converts an unsigned short to a string
+ */
+void hutoa(unsigned char value, char *str){
+   int digits = 6; // 0-> 65535
+   char rev[digits];
+   rev[0] = '\0';
+   int len = 0;
+
+   if (value == 0){
+      str[0] = '0';
+      str[1] = '\0';
+      return;
+   }
+
+   for (int i = 1; value > 0 && i < digits; i++){
+      rev[i] = value % 10;
+      value /= 10;
+      len++;
+   }
+
+   for (int i = 0; i < len; i++)
+      str[i] = '0' + rev[len - i];
+   str[len] = '\0';
+}
+
+/*
+ * Converts a short to a string
+ */
+void htoa(short value, char *str){
+   int digits = 7;     // -32768 -> 32767
+   char rev[digits];
+   rev[0] = '\0';
+   int len = 0;
+
+   if (value == 0){
+      str[0] = '0';
+      str[1] = '\0';
+      return;
+   }
+
+   if (value < 0){
+      htoa(value * -1, str+1);
+      str[0] = '-';
+
+   }
+   else{
+      for (int i = 1; value > 0 && i < digits; i++){
+         rev[i] = value % 10;
+         value /= 10;
+         len++;
+      }
+
+      for (int i = 0; i < len; i++)
+         str[i] = '0' + rev[len - i];
+      str[len] = '\0';
+   }
+}
+
+/*
+ * Converts an unisgned int to hex string 
+ */
+void itohex(unsigned int value, char *str){
+   int digits = 9; // 0x0 -> 0xFFFFFFFF
+   char rev[digits];
+   rev[0] = '\0';
+   int len = 0;
+
+   if (value == 0){
+      str[0] = '0';
+      str[1] = '\0';
+      return;
+   }
+
+   for (int i = 1; value > 0 && i < digits; i++){
+      rev[i] = value % 16;
+      value /= 16;
+      len++;
+   }
+
+   for (int i = 0; i < len; i++){
+      if (rev[len -i] < 10)
+         str[i] = '0' + rev[len - i];
+      else 
+         str[i] = 'a' + (rev[len - i] - 10);
+   }
+   str[len] = '\0';
+}
+
+/*
+ * Converts an unisgned long to hex string 
+ */
+void ltohex(unsigned long value, char *str){
+   int digits = 17; // 0x0 -> 0xFFFFFFFFFFFFFFFF
+   char rev[digits];
+   rev[0] = '\0';
+   int len = 0;
+
+   if (value == 0){
+      str[0] = '0';
+      str[1] = '\0';
+      return;
+   }
+
+   for (int i = 1; value > 0 && i < digits; i++){
+      rev[i] = value % 16;
+      value /= 16;
+      len++;
+   }
+
+   for (int i = 0; i < len; i++){
+      if (rev[len -i] < 10)
+         str[i] = '0' + rev[len - i];
+      else 
+         str[i] = 'a' + (rev[len - i] - 10);
+   }
+   str[len] = '\0';
+}
--- milestone03/src/arch/x86_64/commonlib.h
+++ milestone03/src/arch/x86_64/commonlib.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 void *memset(void *dst, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 size_t strlen(const char *s);
@@ -5,4 +7,11 @@ char *strcpy(char *dest, const char *src);
 int strcmp(const char *s1, const char *s2);
 const char *strchr(const char *s, int c);
 char *strdup(const char *s);
-int printk(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
+void utoa(unsigned char value, char *str);
+void itoa(int value, char *str);
+void htoa(short value, char *str);
+void hutoa(unsigned char value, char *str);
+void lutoa(unsigned long value, char *str);
+void ltoa(long value, char *str);
+void itohex(unsigned int value, char *str);
+void ltohex(unsigned long value, char *str);
