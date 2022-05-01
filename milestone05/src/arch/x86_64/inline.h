6eb787d080d92aa7385ec38cc308cdd8842c57e8
jphaws <jphaws@calpoly.edu>
2022-04-30 20:07:19 -0700

    Added more inline assembly functions

--- milestone05/src/arch/x86_64/inline.h
+++ milestone05/src/arch/x86_64/inline.h
@@ -2,12 +2,13 @@
 #define __INLINE_H__
 
 #include <stdint-gcc.h>
+#include <stdbool.h>
 
 #define CLI __asm__ volatile ("cli");        // used to disable interrupts
 #define STI __asm__ volatile ("sti");        // used to enable interrupts
 
 static inline void lgdt(void *base, uint16_t size){
-   extern struct {
+   struct {
       uint16_t length;
       void *base;
    } __attribute__ ((packed)) GDTR;
@@ -32,6 +33,15 @@ static inline uint8_t inb(uint16_t port){
 
 static inline void io_wait(void){
     outb(0x80, 0);
+}
+
+static inline bool are_interrupts_enabled()
+{
+    unsigned long flags;
+    asm volatile ( "pushf\n\t"
+                   "pop %0"
+                   : "=g"(flags) );
+    return flags & (1 << 9);
 }
                                              //
 #endif
