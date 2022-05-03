e56f1351565614f52db9e6a6c460063239e14e2c
jphaws <jphaws@calpoly.edu>
2022-05-03 00:30:58 -0700

    Updated printing during initalization

--- milestone05/src/arch/x86_64/ps2.c
+++ milestone05/src/arch/x86_64/ps2.c
@@ -43,7 +43,7 @@ void ps2_poll_write(uint16_t port, uint8_t val){
 
 static void disable_devices(){
 
-   printk("Start devices disabled: \n");
+   // printk("Start devices disabled: \n");
    ps2_poll_write(PS2_CMD, PS2_CHANNEL_1);
 
    ps2_poll_write(PS2_CMD, PS2_CHANNEL_2);
@@ -52,8 +52,8 @@ static void disable_devices(){
 static void flush_output_buffer(){
    uint8_t status = inb(PS2_DATA);
 
-   printk("Start buffer flushed:\n");
-   printk("   Status: 0x%x\n", status);
+   // printk("Start buffer flushed:\n");
+   // printk("   Status: 0x%x\n", status);
    while (status & PS2_STATUS_OUTPUT)
       status = inb(PS2_DATA);
 }
@@ -61,63 +61,62 @@ static void flush_output_buffer(){
 static void set_configuration_byte(){
    uint8_t config = inb(PS2_CONTROLLER_CONFIG);
 
-   printk("Start configuration byte:\n");
-   printk("   Configuration byte received: 0x%x\n", config);
+   // printk("Start configuration byte:\n");
+   // printk("   Configuration byte received: 0x%x\n", config);
 
    config |= PS2_DISABLE_CONFIG_MASK_OR;
    config &= PS2_DISABLE_CONFIG_MASK_AND;
 
    ps2_poll_write(PS2_CMD, config);
-   printk("   Configuration byte sent: 0x%x\n", config);
+   // printk("   Configuration byte sent: 0x%x\n", config);
 
 }
 
 static void controller_self_test(){
    uint8_t test;
 
-   printk("Start controller self test:\n");
+   // printk("Start controller self test:\n");
 
    ps2_poll_write(PS2_CMD, 0xAA);
 
-   printk("   Sent PS2 controller self test byte\n");
+   // printk("   Sent PS2 controller self test byte\n");
 
    test = ps2_poll_read(PS2_DATA);
 
    if (test != 0x55)
-      printk("   PS2 Controller failed self test\n");
+      printk("PS2 Controller failed self test\n");
 
-   printk("   Self test byte: 0x%x\n", test);
+   // printk("   Self test byte: 0x%x\n", test);
 
 }
 
 static void interface_tests(){
    uint8_t test;
 
-   printk("Start interface test:\n");
+   // printk("Start interface test:\n");
 
    ps2_poll_write(PS2_CMD, PS2_TEST_FIRST_PORT);
 
-   printk("   Test byte sent\n");
+   // printk("   Test byte sent\n");
 
    test = ps2_poll_read(PS2_DATA);
 
    if (test != 0)
-      printk("   Interface test failed\n");
-   printk("   Interface test byte: 0x%x\n", test);
+      printk("PS2 Controller Interface test failed\n");
+   // printk("   Interface test byte: 0x%x\n", test);
 }
 
 static void enable_devices(){
-   printk("Begin enable PS2 Controller:\n");
+   // printk("Begin enable PS2 Controller:\n");
    ps2_poll_write(PS2_CMD, PS2_ENABLE_FIRST_PORT);
 }
 
 void initialize_PS2_controller(){
-   printk("Begin PS2 Controller Initialization\n");
    disable_devices();
    flush_output_buffer();
    set_configuration_byte();
    controller_self_test();
    interface_tests();
    enable_devices();
-   printk("Finished PS2 Controller Initialization\n");
+   printk("PS2 Controller Initialized\n");
 }
