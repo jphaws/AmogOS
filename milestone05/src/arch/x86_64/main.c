1690e0e6bf1265436221743e871c7b123b6c321c
jphaws <jphaws@calpoly.edu>
2022-05-03 00:33:12 -0700

    Changed initalization screen to display welcome message

--- milestone05/src/arch/x86_64/main.c
+++ milestone05/src/arch/x86_64/main.c
@@ -1,27 +1,35 @@
-#include "vga.h"
-#include "commonlib.h"
 #include "commonio.h"
 #include "ps2.h"
 #include "keyboard.h"
 #include "irq.h"
 #include "gdt.h"
 
-#define ZERO 0
+#define DEBUG 0
+#define KB_INT_NUM 0x21
 
 void kmain(void){
-   int run = ZERO;
-   // int zero = ZERO;
-   while(run);
-
-   initialize_gdt();
-   // run = run/zero;
-   //asm volatile ("int %0"::"N"(0));
+   while(DEBUG);
 
    initialize_PS2_controller();
    initialize_keyboard();
-
+   initialize_gdt();
+   initialize_tss();
    IRQ_init();
+   IRQ_set_handler(KB_INT_NUM, keyboard_read, (void*)0);
+
+   printk("+------------------------------------------------------------------------------+");
+   printk("|                             Welcome to BellardOS                             |");
+   printk("+------------------------------------------------------------------------------+");
+
+   //asm volatile ("int %0"::"N"(0));
+
    while(1){
+      for (int i = 0; i < 400; i++){
+         for (int j = 0; j < 1000000; j++){
+            ;
+         }
+      }
+      printk(".");
    }
 }
 
