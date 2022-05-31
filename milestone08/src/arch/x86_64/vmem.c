cf896c379ca1051dfb8bbafcdfaeaa13bb28a14e
jphaws <jphaws@calpoly.edu>
2022-05-31 16:23:31 -0700

    Fixed compiler errors & warnings

--- milestone08/src/arch/x86_64/vmem.c
+++ milestone08/src/arch/x86_64/vmem.c
@@ -1,7 +1,20 @@
+#include <stdint-gcc.h>
+#include <stdbool.h>
 #include "vmem.h"
 #include "pf.h"
 #include "commonlib.h"
-#include <stdint-gcc.h>
+
+#define VGA_BASE 0xb8000
+
+#define L1_BITS 12
+#define L2_BITS 9
+#define L3_BITS 9
+#define L4_BITS 9
+
+#define PML4_OFFSET(addr) (((addr) >> ((L1_BITS) + (L2_BITS) + (L3_BITS))) & (0x1FF))
+#define PDP3_OFFSET(addr) (((addr) >> ((L1_BITS) + (L2_BITS))) & (0x1FF))
+#define PD2_OFFSET(addr) (((addr) >> (L1_BITS)) & (0x1FF))
+#define PT1_OFFSET(addr) ((addr) & (0xFFF))
 
 struct PT4_entry{
     uint64_t present:1;
@@ -88,7 +101,8 @@ struct virt_addr{
    uint64_t phys_offset:12;
 }__attribute__((packed));
 
-static void map_null(struct PageTable_L3 *pdp3, struct PageTable_L2 *pd2, struct PageTable_L1 *pt1);
+static void map_null();
+static void map_vga();
 //static void set_up_kernel_stack();
 //static void set_up_kernel_heap();
 
@@ -108,30 +122,73 @@ extern void init_virtual_memory(){
    // set_up_kernel_heap();
 }
 
-static void map_null(struct PageTable_L3 *pdp3, struct PageTable_L2 *pd2, struct PageTable_L1 *pt1){
-   pdp3->entries[0].p2_addr = (uint64_t)pd2;
-   pdp3->entries[0].present = 1;
 
-   pd2->entries[0].p1_addr = (uint64_t)pt1;
-   pd2->entries[0].present = 1;
+static void map_addr(uint64_t addr, bool present, bool nx){
+   struct PageTable_L3* pdp3;
+   struct PageTable_L2* pd2;
+   struct PageTable_L1* pt1;
+   uint16_t l4_offset = PML4_OFFSET(addr);
+   uint16_t l3_offset = PDP3_OFFSET(addr);
+   uint16_t l2_offset = PD2_OFFSET(addr);
+   uint16_t l1_offset = PT1_OFFSET(addr);
 
-   pt1->entries[0].present = 0;
-   pt1->entries[0].nx = 1;
+   // create new pdp3 entry if not present 
+   if(!pml4->entries[l4_offset].present){
+      pdp3 = MMU_pf_alloc();
+      memset(pdp3, 0, PAGE_SIZE);
+      pml4->entries[l4_offset].p3_addr = (uint64_t)pdp3;
+      pml4->entries[l4_offset].present = 1;
+   }
+   pdp3 = (struct PageTable_L3*)((uint64_t)(pml4->entries[l4_offset].p3_addr));
+
+   // create new pd2 entry if not present 
+   if(!pdp3->entries[l3_offset].present){
+      pd2 = MMU_pf_alloc();
+      memset(pd2, 0, PAGE_SIZE);
+      pdp3->entries[l3_offset].p2_addr = (uint64_t)pd2;
+      pdp3->entries[l3_offset].present = 1;
+   }
+   pd2 = (struct PageTable_L2*)((uint64_t)(pdp3->entries[l3_offset].p2_addr));
+
+   // create new pt1 entry if not present 
+   if(!pd2->entries[l2_offset].present){
+      pt1 = MMU_pf_alloc();
+      memset(pt1, 0, PAGE_SIZE);
+      pd2->entries[l2_offset].p1_addr = (uint64_t)pt1;
+      pd2->entries[l2_offset].present = 1;
+   }
+   pt1 = (struct PageTable_L1*)((uint64_t)(pd2->entries[l2_offset].p1_addr));
+   pt1->entries[l1_offset].phys_addr = addr;
+   if (present)
+      pt1->entries[l1_offset].present = 1;
+   if (nx)
+      pt1->entries[l1_offset].nx = 1;
+}
+
+static void map_null(){
+   map_addr(0, false, true);
+}
+
+static void map_vga(){
+   map_addr(VGA_BASE, true, true);
 }
 
 static void identity_map(){
    // alloc a page for L4 table & set to zero
    pml4 = MMU_pf_alloc();
    memset(pml4, 0, PAGE_SIZE);
+
+   /*
    struct PageTable_L3* pdp3 = MMU_pf_alloc();
    memset(pdp3, 0, PAGE_SIZE);
    struct PageTable_L2* pd2 = MMU_pf_alloc();
    memset(pd2, 0, PAGE_SIZE);
    struct PageTable_L1* pt1 = MMU_pf_alloc();
    memset(pt1, 0, PAGE_SIZE);
+   */
 
-   map_null(pdp3, pd2, pt1);
-   // map_vga();
+   map_null();
+   map_vga();
 
    /*
    for (int i = 0; i < 512; i++){
